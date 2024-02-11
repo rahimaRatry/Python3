5fc552651f151809d68c726730d43df0a48d2728
Rahima Akter Ratry <115058821+rahimaRatry@users.noreply.github.com>
2024-02-11 11:54:56 +0600

    Add files via upload

---
+++ 11-dict-iterate .py
@@ -0,0 +1,7 @@
+# To read the items of a dictionary one by one
+
+a = {'name':'Md Shajjad Hossain','nickname':'Shojib','email':'shajjad@gmail.com','phone':'01234563143'}
+print(a)
+print(type(a))
+for item in a :
+    print(item) 
---
+++ 12-string-iterate .py
@@ -0,0 +1,5 @@
+# to read the letters of a string one by one
+
+a = 'Python'
+for letter in a:
+    print(letter) 
