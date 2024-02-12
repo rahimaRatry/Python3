ed0a81df47d1a69c48a25f055c46d7f533712237
Rahima Akter Ratry <115058821+rahimaRatry@users.noreply.github.com>
2024-02-12 10:59:49 +0600

    Add files via upload

---
+++ 24-function .py
@@ -0,0 +1,18 @@
+def print_my_name(myname):
+    #this will print the given name
+    print("The given name is:",myname)
+    return
+print_my_name("Thomas") 
+
+def print_name(name):
+    #this will also print the given name
+    print("The another given name is:", name)
+    return
+name = "Edison"
+print_name(name) 
+
+def add(a, b, c):
+    #this function will add three numbers
+    return a+b+c
+temp = add(1, 2, 3)
+print("Sum of three numbers:",temp) 
