4324123ce41623ab40eb5ba70945b9fad3e79382
Rahima Akter Ratry <115058821+rahimaRatry@users.noreply.github.com>
2024-02-15 10:57:30 +0600

    Update 26-recursion .py

--- 26-recursion .py
+++ 26-recursion .py
@@ -1,3 +1,7 @@
+"""Recursive functions are functions that calls itself.
+It is always made up of 2 portions, the base case and the recursive case. 
+The base case is the condition to stop the recursion. The recursive case is the part where the function calls on itself."""
+
 def counter(num):
     print(num)
     num += 1
