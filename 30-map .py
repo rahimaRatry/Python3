e21c8859f5bbfa00acfad0f953d17a7e38f0989a
Rahima Akter Ratry <115058821+rahimaRatry@users.noreply.github.com>
2024-02-15 11:06:25 +0600

    Update 30-map .py

--- 30-map .py
+++ 30-map .py
@@ -1,6 +1,8 @@
-my_list = [2, 3, 4, 5, 6, 7]
-def square(x) :
+# map is a built-in function.we need to take 2 arguments here, first one is Function and second one is Iterator Object.
+
+my_list = [2, 3, 4, 5, 6, 7] # Iterator Object
+def square(x) :  #Function
     return x * x
-new_list = map(square, my_list)
-print(new_list)
-print(list(new_list))
+new_list = map(square, my_list) 
+print(new_list) 
+print(list(new_list)) 
