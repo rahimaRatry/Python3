d22758d1017b9ada14c216ac33014df734799d98
Rahima Akter Ratry <115058821+rahimaRatry@users.noreply.github.com>
2024-02-16 09:20:30 +0600

    Add files via upload

---
+++ 35-file .py
@@ -0,0 +1,4 @@
+
+my_file = open('test.txt','w')  #here we can open the file in write mode by using 'w'.
+my_file.write("I am Rahima Akter Ratry.")  #by using this method we can write in our file.
+my_file.close() 
