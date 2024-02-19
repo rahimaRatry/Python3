4da6e91001f7efaa07a67313a18ad9d6f09165e6
Rahima Akter Ratry <115058821+rahimaRatry@users.noreply.github.com>
2024-02-19 11:43:10 +0600

    Add files via upload

---
+++ 33-problem.py
@@ -0,0 +1,9 @@
+""" Take a number from user and check if that Even or Odd number."""
+
+print("Please input a number:")
+number = int(input())
+
+if number % 2 == 0: 
+    print("The number is Even number.")
+else:
+    print("The number is Odd number.")
