68af91834def3bb4bf123cbaf810a47802b86e06
Rahima Akter Ratry <115058821+rahimaRatry@users.noreply.github.com>
2024-03-20 13:59:57 +0600

    Add files via upload

---
+++ 46-class .py
@@ -0,0 +1,16 @@
+# here is a class with some functions named driving(), music(),fuel(),horn().. 
+
+class WaltonUsta:
+    """ Our new car class"""
+
+    def driving(self):
+        run_the_car 
+    
+    def music(self):
+        run_the_music
+
+    def fuel(self):
+        load_the_fuel
+    
+    def horn(self):
+        make_sound_pollution 
